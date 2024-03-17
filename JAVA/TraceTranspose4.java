df72c5265f31fd35f1743726475110ce3e0e1612
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-03-17 23:41:04 +0530

    update

--- JAVA/TraceTranspose4.java
+++ JAVA/TraceTranspose4.java
@@ -1,68 +1,46 @@
 // Write a program to find the trace and transpose of a matrix.
 
-import java.util.Scanner;
+import java.util.*;
 
 public class TraceTranspose4 {
-    public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter the number of rows: ");
-		int rows = scanner.nextInt();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter the number of columns: ");
-		int cols = scanner.nextInt();
+        int sum = 0;
 
-		int[][] matrix = new int[rows][cols];
+        System.out.println("Rows: ");
+        int rows = sc.nextInt();
 
-		System.out.println("Enter the elements of the matrix: ");
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				matrix[i][j] = scanner.nextInt();
-			}
-		}
+        System.out.println("Cols: ");
+        int cols = sc.nextInt();
 
-    System.out.println("Original matrix: ");
+        int matrix[][] = new int[rows][cols];
 
-    displayMatrix(matrix);
-
-		int trace = findTrace(matrix);
-		System.out.println("Trace of the matrix: " + trace);
-
-		int[][] transpose = findTranspose(matrix);
-		System.out.println("Transpose of the matrix: ");
-		displayMatrix(transpose);
-
-		scanner.close();
-	}
-
-    public static int findTrace(int[][] matrix) {
-        int trace = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            trace += matrix[i][i];
-        }
-        return trace;
-    }
-
-    public static int[][] findTranspose(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        int[][] transpose = new int[cols][rows];
-
-        for (int i = 0; i < cols; i++) {
-            for (int j = 0; j < rows; j++) {
-                transpose[i][j] = matrix[j][i];
+        System.out.println("Matrix:");
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
             }
         }
-        return transpose;
-    }
 
-    public static void displayMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int num : row) {
-                System.out.print(num + " ");
+        System.out.println("Transpose:");
+        for(int i = 0; i < cols; i++) {
+            for(int j = 0; j < rows; j++) {
+                System.out.print(" " + matrix[j][i]);
             }
             System.out.println();
+        }
+
+        if(rows == cols) {
+            for(int i = 0; i < cols; i++) {
+                for(int j = 0; j < rows; j++) {
+                    if(i == j) {
+                        sum += matrix[i][j];
+                    }
+                }
+            }
+
+            System.out.println("Trace: " + sum);
         }
     }
 }
