3eaba9953acf239236472bf115cf52af0df2a8df
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-03-18 00:34:03 +0530

    updte

--- JAVA/ObjectCount7.java
+++ JAVA/ObjectCount7.java
@@ -1,20 +1,22 @@
 // Write a program to count and display total number of objects created to a class. (Hint: static members).
 
 class Object {
-    static int count;
+    static int count; // Static variable to count objects
 
     Object() {
-        count++;
+        count++; // Increment count each time an object is created
     }
 }
 
 public class ObjectCount7 {
     public static void main(String args[]) {
+        // Creating four objects of the Object class
         Object shit = new Object();
         Object shit1 = new Object();
         Object shit2 = new Object();
         Object shit3 = new Object();
 
+        // Printing the count of objects created
         System.out.println(Object.count);
     }
 }
