c9b69e09ecce8f7926a941cfc51fab4dd197ed9b
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-03-21 00:20:56 +0530

    update

--- ANDROID/9-CheckBox/checkbox.java
+++ ANDROID/9-CheckBox/checkbox.java
@@ -10,29 +10,30 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
-    CheckBox chk1, chk2, chk3, chk4;
+public class MainActivity extends Activity {
+    CheckBox chk1, chk2;
     Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         chk1 = (CheckBox) findViewById(R.id.checkBox1);
         chk2 = (CheckBox) findViewById(R.id.checkBox2);
-        chk3 = (CheckBox) findViewById(R.id.checkBox3);
-        chk4 = (CheckBox) findViewById(R.id.checkBox4);
-        btn = (Button) findViewById(R.id.button1);
-        btn.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View arg0) {
-        // TODO Auto-generated method stub StringBuffer strb = new StringBuffer();
-        strb.append("Android: ").append(chk1.isChecked());
-        strb.append("\nJava: ").append(chk2.isChecked());
-        strb.append("\nPHP: ").append(chk3.isChecked());
-        strb.append("\nShell Script: ").append(chk4.isChecked());
-        Toast.makeText(MainActivity.this, strb.toString(), Toast.LENGTH_LONG).show();
+        btn = (Button) findViewById(R.id.button1);
+
+        btn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                StringBuffer s = new StringBuffer();
+                s.append("SBQ: ").append(chk1.isChecked());
+                s.append("\nMahir: ").append(chk2.isChecked());
+
+                Toast.makeText(MainActivity.this, s.toString(), 500).show();
+            }
+        });
     }
 }
