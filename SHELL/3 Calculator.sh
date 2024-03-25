0fe00f45c22000c0f21388b17e3f3179db4adcf2
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-03-25 23:34:09 +0530

    updated

--- SHELL/3 Calculator.sh
+++ SHELL/3 Calculator.sh
@@ -11,7 +11,6 @@ read n2
 
 while [ $i = "y" ]
 do
-<<<<<<< HEAD
 	echo "1. Addition"
 	echo "2. Subtraction"
 	echo "3. Multiplication"
@@ -38,29 +37,4 @@ do
 	then
 		exit
 	fi
-=======
-    echo "1.Addition"
-    echo "2.Subtraction"
-    echo "3.Multiplication"
-    echo "4.Division"
-    echo "Enter your choice"
-    read ch
-    case $ch in
-        1)sum=`expr $n1 + $n2`
-        echo "Sum ="$sum;;
-        2)sub=`expr $n1 - $n2`
-        echo "Sub = "$sub;;
-        3)mul=`expr $n1 \* $n2`
-        echo "Mul = "$mul;;
-        4)div=`expr $n1 / $n2`
-        echo "Div = "$div;;
-        *)echo "Invalid choice";;
-    esac
-    echo "Do u want to continue ?"
-    read i
-    if [ $i != "y" ]
-    then
-        exit
-    fi
->>>>>>> 16de1c1091d7eedfbbbcadc744d5f520d3318d5a
 done
--- SHELL/7 ReverseNumber.sh
+++ SHELL/7 ReverseNumber.sh
@@ -6,7 +6,6 @@ read n
 digit=0
 reverse=0
 
-<<<<<<< HEAD
 while [ $n -gt 0 ]
 do
 	digit=$(( $n % 10 ))
@@ -15,18 +14,3 @@ do
 done
 
 echo "The reverse of the digits is " $reverse
-=======
-echo "Enter a number: "
-read num
-
-reverse=0
-
-while [ $num -gt 0 ]
-do
-    remainder=$(( $num % 10 ))
-    reverse=$(( $reverse * 10 + $remainder ))
-    num=$(( $num / 10 ))
-done
-
-echo "Reverse of the number is: $reverse"
->>>>>>> 16de1c1091d7eedfbbbcadc744d5f520d3318d5a
