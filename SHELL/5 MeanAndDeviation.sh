16de1c1091d7eedfbbbcadc744d5f520d3318d5a
Sabiq Hashil <99120128+SabiqHashil@users.noreply.github.com>
2024-03-25 21:25:57 +0530

    Update 5 MeanAndDeviation.sh

--- SHELL/5 MeanAndDeviation.sh
+++ SHELL/5 MeanAndDeviation.sh
@@ -2,16 +2,14 @@
 
 
 
-
-
 echo "Enter three integers with space between"
 read a b c
-sum=`expr $a + $b + $c`
-mean=`expr $sum / 3`
+sum=$((a + b + c))
+mean=$((sum / 3))
 aa=$((($a - $mean) * ($a - $mean)))
 bb=$((($b - $mean) * ($b - $mean)))
 cc=$((($c - $mean) * ($c - $mean)))
-sd=$( echo "sqrt(($aa + $bb + $cc) / 3)" | bc -l )
-echo "sum=$sum"
-echo "mean=$mean"
-echo "Sd=$sd"
+sd=$(echo "sqrt(($aa + $bb + $cc) / 3)" | bc -l)
+echo "Sum = $sum"
+echo "Mean = $mean"
+echo "SD = $sd"
